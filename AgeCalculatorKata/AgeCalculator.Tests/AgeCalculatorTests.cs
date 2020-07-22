7cb6eb3564c82dbf3b05bc813b9a454b5b730062
Sahil Muniram <sahil.muniram@chillisoft.co.za>
2020-07-22 12:07:57 +0200

    Age Calculator Kata Code Base With Tests.

---
+++ AgeCalculatorKata/AgeCalculator.Tests/AgeCalculatorTests.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AgeCalculator.Tests
+{
+    [TestFixture]
+    public class AgeCalculatorTests
+    {
+        private AgeCalculator CreateAgeCalculator()
+        {
+            return new AgeCalculator();
+        }
+
+        [Test]
+        public void CalculateAge_GivenDateOfBirthIsInFuture_ThrowException()
+        {
+            //Arrange 
+            var dateOfBirth = new DateTime(2021, 1, 1);
+            var targetDate = new DateTime(2020, 1, 1);
+            const string expectedOutput = "Unborn, date of birth is in the future.";
+
+            //Act             
+            var output = Assert.Throws<Exception>(() => CreateAgeCalculator().CalculateAge(dateOfBirth, targetDate));
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output.Message);
+        }
+
+        [Test]
+        public void CalculateAge_GivenDateOfBirthIsToday_Return0()
+        {
+            //Arrange 
+            var dateOfBirth = DateTime.Now.Date;
+            var targetDate = DateTime.Now.Date;
+            const int expectedOutput = 0;
+
+            //Act             
+            var output = CreateAgeCalculator().CalculateAge(dateOfBirth, targetDate);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void CalculateAge_GivenDateOfBirthIsSameMonthAsTargetDateButDifferentDay_ReturnAgeInDays()
+        {
+            //Arrange 
+            var dateOfBirth = new DateTime(2020,6,1);
+            var targetDate = new DateTime(2020, 6, 15);
+            const int expectedOutput = 14;
+
+            //Act             
+            var output = CreateAgeCalculator().CalculateAge(dateOfBirth, targetDate);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void CalculateAge_GivenDateOfBirthIsSameYearAsTargetDate_ReturnAgeInMonths()
+        {
+            //Arrange 
+            var dateOfBirth = new DateTime(2020, 06, 01);
+            var targetDate = new DateTime(2020, 12, 01);
+            const int expectedOutput = 6;
+
+            //Act             
+            var output = CreateAgeCalculator().CalculateAge(dateOfBirth, targetDate);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void CalculateAge_GivenBirthdayIsOneDayBeforeInDifferentMonth_ReturnAge()
+        {
+            //Arrange 
+            var dateOfBirth = new DateTime(2010, 12, 1);
+            var targetDate = new DateTime(2020, 11, 30);
+            const int expectedOutput = 9;
+
+            //Act             
+            var output = CreateAgeCalculator().CalculateAge(dateOfBirth, targetDate);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void CalculateAge_GivenBirthdayIsOneDayBeforeInSameMonth_ReturnAge()
+        {
+            //Arrange 
+            var dateOfBirth = new DateTime(2010, 12, 2);
+            var targetDate = new DateTime(2020, 12, 1);
+            const int expectedOutput = 9;
+
+            //Act             
+            var output = CreateAgeCalculator().CalculateAge(dateOfBirth, targetDate);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void CalculateAge_GivenBirthdayIsOneMonthAway_ReturnAge()
+        {
+            //Arrange 
+            var dateOfBirth = new DateTime(2010, 12, 1);
+            var targetDate = new DateTime(2020, 11, 1);
+            const int expectedOutput = 9;
+
+            //Act             
+            var output = CreateAgeCalculator().CalculateAge(dateOfBirth, targetDate);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void CalculateAge_GivenBirthdayIsOnTheDayOfTargetDate_ReturnAge()
+        {
+            //Arrange 
+            var dateOfBirth = new DateTime(2010, 12, 1);
+            var targetDate = new DateTime(2020, 12, 1);
+            const int expectedOutput = 10;
+
+            //Act             
+            var output = CreateAgeCalculator().CalculateAge(dateOfBirth, targetDate);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void CalculateAge_GivenOneDayAfterBirthday_ReturnAge()
+        {
+            //Arrange 
+            var dateOfBirth = new DateTime(2010, 12, 1);
+            var targetDate = new DateTime(2020, 12, 2);
+            const int expectedOutput = 10;
+
+            //Act             
+            var output = CreateAgeCalculator().CalculateAge(dateOfBirth, targetDate);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void CalculateAge_GivenDateOfBirthIsLeapYearAndTargetDateIsLeapYear_ReturnAge()
+        {
+            //Arrange 
+            var dateOfBirth = new DateTime(2008, 2, 29);
+            var targetDate = new DateTime(2020, 2, 29);
+            const int expectedOutput = 12;
+
+            //Act             
+            var output = CreateAgeCalculator().CalculateAge(dateOfBirth, targetDate);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void CalculateAge_GivenDateOfBirthIsLeapYearAndTargetDateIsNotLeapYearButSameMonth_ReturnAge()
+        {
+            //Arrange 
+            var dateOfBirth = new DateTime(2008, 2, 29);
+            var targetDate = new DateTime(2019, 2, 28);
+            const int expectedOutput = 10;
+
+            //Act             
+            var output = CreateAgeCalculator().CalculateAge(dateOfBirth, targetDate);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void CalculateAge_GivenDateOfBirthIsLeapYearAndTargetDateIsNotLeapYearAndOneMonthAfterBirthday_ReturnAge()
+        {
+            //Arrange 
+            var dateOfBirth = new DateTime(2008, 2, 29);
+            var targetDate = new DateTime(2019, 3, 1);
+            const int expectedOutput = 11;
+
+            //Act             
+            var output = CreateAgeCalculator().CalculateAge(dateOfBirth, targetDate);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void CalculateAge_GivenDateOfBirthIsLeapYearAndTargetDateIsNotLeapYearAndOneMonthBeforeBirthday_ReturnAge()
+        {
+            //Arrange 
+            var dateOfBirth = new DateTime(2008, 2, 29);
+            var targetDate = new DateTime(2019, 1, 15);
+            const int expectedOutput = 10;
+
+            //Act             
+            var output = CreateAgeCalculator().CalculateAge(dateOfBirth, targetDate);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output);
+        }
+    }
+}
---
+++ AgeCalculatorKata/AgeCalculator/AgeCalculator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace AgeCalculator
+{
+    public class AgeCalculator
+    {
+        public int CalculateAge(DateTime dateOfBirth, DateTime targetDate)
+        {
+            if (dateOfBirth > targetDate)
+                throw new Exception("Unborn, date of birth is in the future.");
+
+            if (IsSameYearMonthDay(dateOfBirth, targetDate))
+                return 0;
+
+            if (IsSameYearAndMonth(dateOfBirth, targetDate))
+                return GetDifferenceInDays(dateOfBirth, targetDate);
+
+            if (IsSameYear(dateOfBirth, targetDate))
+                return GetDifferenceInMonths(dateOfBirth, targetDate);
+
+            return CalculateExactAge(dateOfBirth, targetDate);
+        }
+
+        private int CalculateExactAge(DateTime dateOfBirth, DateTime targetDate)
+        {
+            var age = GetDifferenceInYears(dateOfBirth, targetDate);
+            var differenceInMonths = GetDifferenceInMonths(dateOfBirth, targetDate);
+
+            if (DateTime.IsLeapYear(dateOfBirth.Year))
+            {
+                if (differenceInMonths <= 0 && dateOfBirth.Day > targetDate.Day)
+                    age--;
+                return age;
+            }
+
+            if (differenceInMonths != 0 || GetDifferenceInDays(dateOfBirth, targetDate) != 0)
+                age--;
+
+            if (dateOfBirth.Month == targetDate.Month && targetDate.Day > dateOfBirth.Day)
+                age++;
+
+            return age;
+        }
+
+        private bool IsSameYearMonthDay(DateTime dateOfBirth, DateTime targetDate)
+        {
+            return dateOfBirth == targetDate;
+        }
+
+        private bool IsSameYearAndMonth(DateTime dateOfBirth, DateTime targetDate)
+        {
+            return (dateOfBirth.Year == targetDate.Year && dateOfBirth.Month == targetDate.Month);
+        }
+
+        private bool IsSameYear(DateTime dateOfBirth, DateTime targetDate)
+        {
+            return (dateOfBirth.Year == targetDate.Year);
+        }
+
+        private int GetDifferenceInDays(in DateTime dateOfBirth, in DateTime targetDate)
+        {
+            var days = targetDate.Day - dateOfBirth.Day;
+            return days;
+        }
+
+        private int GetDifferenceInMonths(in DateTime dateOfBirth, in DateTime targetDate)
+        {
+            var months = targetDate.Month - dateOfBirth.Month;
+            return months;
+        }
+
+        private int GetDifferenceInYears(DateTime dateOfBirth, DateTime targetDate)
+        {
+            var years = targetDate.Year - dateOfBirth.Year;
+            return years;
+        }
+    }
+}
